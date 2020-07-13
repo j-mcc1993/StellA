904446ab79fd136fb316325484e6aa2001e87b12
j-mcc1993 <j.mcc3093@gmail.com>
2020-07-13 00:33:22 -0700

    Tweaking tolerance and rounding

--- StellariumTracker.py
+++ StellariumTracker.py
@@ -1,9 +1,9 @@
 import serial, requests
 
 # Locals
-pi    = 3.14159
-rad   = pi/180.0
-tol   = 0.01
+pi  = 3.14159
+rad = pi/180.0
+tol = 0.025
 
 # Position vector
 azAlt = {'az' : pi, 'alt' : 0.0}
@@ -13,7 +13,7 @@ def updateView():
     requests.post("http://localhost:8090/api/main/view", data=azAlt)
 
 # Initialize serial connection with Arduino
-arduino = serial.Serial('/dev/cu.usbmodem14201', 9600, timeout=.1)
+arduino = serial.Serial('/dev/cu.usbmodem14201', 9600, timeout=1)
 
 # Initialize Stellarium view
 updateView()
@@ -22,18 +22,22 @@ updateView()
 while True:
     data = arduino.readline()[:-2] # remove newlines
     if data:
-        data = data.decode('utf8')
-        print(data)
+        data = data.decode('utf8') # translate raw bytes to str
+        data = [round(float(x)*rad, 2) for x in data.split()]
 
-        data = [float(x)*rad for x in data.split()]
+        # Check if there's been a read-error
         if len(data) != 2:
             print("Error! Incomplete line of data read")
             continue
+
+        # Display alt az to console for debugging
+        print("az: " + str(data[0]) + "  alt: " + str(data[1]))
         
         # If full line of data was read, obtain alt/az values
         delt_az  = abs(azAlt['az'] - data[0])
         delt_alt = abs(azAlt['alt'] - data[1])
 
+        # If delta is large enough, update the view
         if delt_az > tol or delt_alt > tol:
             azAlt['az']  = data[0]
             azAlt['alt'] = data[1]
