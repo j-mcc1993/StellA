c7996891e4987bef34c8c24d75e34d60579a68c5
j-mcc1993 <j.mcc3093@gmail.com>
2020-07-21 23:55:10 -0700

    Updated StellA to work with Nano 33 BLE board, now includes dewpoint monitoring system

--- StellA.py
+++ StellA.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python3
 import asyncio
+import aioconsole
 import platform
 import requests
 import serial
@@ -7,18 +8,59 @@ import struct
 from bleak import BleakClient, BleakError
 
 # For establishing bluetooth connection
-char_uuid = "19b10001-e8f2-537e-4f6c-d104768a1214"
-dev_uuid  = "E5DD24C3-3942-453F-91E5-5568D1BB4908"
+azalt_uuid = "19b10001-e8f2-537e-4f6c-d104768a1214"
+thd_uuid   = "19b10002-e8f2-537e-4f6c-d104768a1214"
+dev_uuid   = "C2AC7E0A-F077-4D23-BEFC-23C47C933A6C"
 
 # Globals
-pi    = 3.14159
-rad   = pi/180.0
-brkpt = 4
-north = 37.385050 
-delay = 60.0
+pi        = 3.14159
+rad       = pi/180.0
+deg       = 180.0/pi
+brkpt     = 4
+brkpt2        = 8
+north     = 37.385050 
+delay     = 60.0
+tol_az    = .001
+tol_al    = .0005
+az_off    = 0.0
+al_off    = 0.0
 
 # Position vector
-azAlt = {'az' : pi, 'alt' : north}
+azAlt = {'az' : pi, 'alt' : north*rad}
+
+# Exit
+async def exit():
+    x = await aioconsole.ainput(">>> ")
+
+# Get temperature, humidity and dewpoint
+def getTHD(data):
+    print("Getting temperature data...")
+
+    # Get tmp, humidity and dewpoint
+    tmp = round(float(struct.unpack('f', data[:brkpt])[0]), 1)
+    hum = round(float(struct.unpack('f', data[brkpt:brkpt2])[0]), 1)
+    dew = round(float(struct.unpack('f', data[brkpt2:])[0]), 1)
+    
+
+    print("Temp: " + str(tmp) + "F    Humidity: " + 
+          str(hum) + "%    Dewpoint: " + str(dew) + "F")
+          
+
+    
+# Reset offsets
+def calibrate():
+    # Offsets
+    global az_off
+    global al_off
+
+    # Calculate offset from polaris
+    print("Calculating calibration offsets from Polaris...")
+
+    az_off = azAlt['az'] - pi
+    al_off = azAlt['alt'] - north*rad
+
+    print("az_off: " + str(az_off))
+    print("al_off: " + str(al_off))
 
 # Function to update Stellarium view via HTTP request
 def updateView():
@@ -26,18 +68,32 @@ def updateView():
 
 # Handles updates to azAlt data
 def notification_handler(sender, data):
-    az  = float(struct.unpack('f', data[:brkpt])[0])
-    alt = float(struct.unpack('f', data[brkpt:])[0])
-    print("az: " + str(round(az,1)) + "    alt: " + str(round(alt,1)))
+    # To update or not
+    update = False
 
-    # Convert to radians
-    az  *= rad
-    alt *= rad
-    azAlt['az']  = az
-    azAlt['alt'] = alt
+    # Get az and alt and convert to radians
+    az  = round(float(struct.unpack('f', data[:brkpt])[0])*rad + az_off, 4)
+    alt = round(float(struct.unpack('f', data[brkpt:])[0])*rad + al_off, 4)
+
+    # Check for overflow from offset addition
+    if az < 0.0 or az > 360.0:
+        az = az % 360.0
+    if alt > 90.0:
+        alt -= 2*(alt%90.0)
+        
+
+    # Check if an update is necessary
+    if abs(azAlt['az'] - az) > tol_az:
+        update = True
+        azAlt['az'] = az
+    
+    if abs(azAlt['alt'] - alt) > tol_al:
+        update = True
+        azAlt['alt'] = alt
 
     # Update Stellarium view
-    updateView()
+    if update:
+        updateView()
     
 
 # Establish BLE connection and loop continuously
@@ -45,13 +101,23 @@ async def get_azAlt(dev_uuid: str, loop: asyncio.AbstractEventLoop):
     async with BleakClient(dev_uuid, loop=loop) as client:
         # Ensure connection is established
         await client.is_connected()
-        
+        print("Client Connected!")
+
         # Enable notification and assign handler
-        await client.start_notify(char_uuid, notification_handler)
+        await client.start_notify(azalt_uuid, notification_handler)
 
         # Wait for notifications
         while True:
-            await asyncio.sleep(delay)
+            # Await user input
+            x = await aioconsole.ainput(">>> ")
+            x = x.lower()
+
+            if x == 'c' or x == "calibrate":
+                calibrate()
+
+            if x == 't' or x == "temperature":
+                tmp = await client.read_gatt_char(thd_uuid)
+                getTHD(tmp)
 
 
 try:
@@ -60,6 +126,9 @@ try:
 except KeyboardInterrupt:
     loop.stop()
     print("\nExiting...")
-except BleakError:
+except BleakError as e:
     loop.stop()
-    print("\nDevice couldn't be found, please reset and try again.\n")
+    print("\n" + str(e) + "\n")
+except requests.exceptions.RequestException as e:
+    loop.stop()
+    print("\n" + str(e) + "\n")
